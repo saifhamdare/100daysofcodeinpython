ff56dc798d5531e35626580d5ace1452d9f23740
saif <40684785+saifhamdare@users.noreply.github.com>
2020-12-28 21:27:15 +0530

    Add files via upload

---
+++ 100daysofcodingchallege/day 1/bmi.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+h=float(height)
+w=float(weight)
+#Write your code below this line 👇
+bmi=w/(h*h)
+bmi=int(bmi)
+print(bmi)
